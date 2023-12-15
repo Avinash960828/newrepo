6fe53428e487372278b002d123fbfc9be357a9e9
avinash960828 <ak6955191@gmail.com>
2023-12-15 15:13:54 +0530

    string_number in js added

---
+++ string_number.js
@@ -0,0 +1,36 @@
+let str1="hello";
+let str2=" avinash";
+let str3=str1+str2;
+// console.log(str3);
+
+console.log(1+"2"); //==> 12
+console.log("1"+2+2); //==> 122
+console.log(1+2+"3"); //==> 32
+
+
+console.log(true);
+
+let num1,num2,mum3
+num1=num2=num3=2+2;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+// prefix increment
+
+let x=3;
+let y= ++x;//=> here y =4 and x=4
+
+//postfix increment 
+
+let a=3;
+let b=a++;// => here b=3 and a will be 4
+
+console.log(a,b);
+console.log(x,y);
+
+
+
