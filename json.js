55b6fca18d246d8c430c4478d2f78218216cfa6b
avinash960828 <ak6955191@gmail.com>
2023-12-21 18:42:47 +0530

    json

---
+++ json.js
@@ -0,0 +1,23 @@
+const course= {
+    coursename:"js in hindi",
+price:"99",
+coysreinstructer: "avinash"
+}
+// course.coysreinstructer
+const {coysreinstructer}=course
+console.log(coysreinstructer );
+
+
+/////json/////////
+// {
+//     "name": "avinash kumar"
+//    " coursename":"js in hindi"
+//    " price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+
+]
