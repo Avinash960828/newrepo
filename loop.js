c43c288e60d36d2700261192999cc4391b75c2ca
avinash960828 <ak6955191@gmail.com>
2023-12-27 08:47:43 +0530

    loop

---
+++ loop.js
@@ -0,0 +1,60 @@
+// for loop
+
+// for (let index= 0;index<=10; index++) {
+//     const element = index
+//     if(element==5){
+//         console.log("5 is best");
+//     }
+//     console.log(element);
+    
+// }
+
+
+// for (let i = 0; i <=10; i++) {
+//     console.log("outer loop");
+//     console.log(i);
+//     for (let j = 0; j <=10; j++) {
+//       console.log("inner loop");
+//         console.log(j);
+//     }
+    
+// }
+
+// for (let i = 1; i <=10; i++) {
+    
+//     for (let j = 1; j <=10; j++) {
+//      console.log(i+ " * "+j+ "=" + " "+i*j);
+//     }
+    
+// }
+
+
+
+//////break and continue///////
+
+for (let i = 0; i <=20; i++) {
+    if (i==5) {
+        console.log("5 detected");
+        //break
+ continue;
+    }
+   console.log(`value of i is ${i}`);
+    
+}
+
+
+
+
+////////while////////////
+let i=0
+while (i<=10) {
+    console.log(`value is ${i}`);
+    i++
+}
+
+
+
+
+
+
+
